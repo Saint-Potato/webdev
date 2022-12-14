4ba0ecad070e0179933adf303529edc5cc24fef4
Aryan Thapliyal <94701379+Saint-Potato@users.noreply.github.com>
2022-12-15 01:27:50 +0530

    Add files via upload

---
+++ PHP1.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+</head>
+<body>
+ <?php
+ echo"The pattern in the question is:";
+ echo"<br><br>";
+ $n=4;
+ for($i=0; $i<$n; $i++){
+ for($j=0;$j<2*$n-1;$j++){
+ if(($i+$j)<$n || ($i+$j)>= $n+2*$i-1)echo"*";
+ else echo"&nbsp;&nbsp;";
+ }
+ echo "<br>";
+ }
+ for($i=1;$i<$n;$i++){
+ for($j=0; $j<2*$n-1; $j++){
+ if($j<=$i || ($i+$j)>= $n+($n/2))echo"*";
+ else echo "&nbsp;&nbsp;";
+ }
+ echo"<br>";
+ }
+ ?>
+</body>
+</html>
---
+++ PHP3.php
@@ -0,0 +1,6 @@
+<?php
+$background_colour =
+    array('white','green','red','yellow','blue','orange','black','purple','peach','cyan');
+$rand_background = $background_colour[array_rand($background_colour)];
+
+?>
